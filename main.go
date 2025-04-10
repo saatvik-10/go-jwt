4355c84dedb3242b8e5adb69efc0bc6145169f55
saatvik-10 <saatvikmadan529@gmail.com>
2025-04-10 18:41:06 +0530

    added routes

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"jwt/database"
+	"jwt/routes"
 	"log"
 
 	"github.com/gofiber/fiber/v3"
@@ -12,9 +13,7 @@ func main() {
 
 	app := fiber.New()
 
-	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	routes.Setup(app)
 
 	log.Fatal(app.Listen(":3000"))
 }
