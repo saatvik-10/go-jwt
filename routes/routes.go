895eda884f256aac3b1745db2fe54872e313cc7f
saatvik-10 <saatvikmadan529@gmail.com>
2025-04-10 19:00:37 +0530

    added controllers

--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,10 @@
 package routes
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"github.com/gofiber/fiber/v3"
+	"jwt/controllers"
+)
 
 func Setup(app *fiber.App) {
-	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	app.Get("/", controllers.Hello)
 }
