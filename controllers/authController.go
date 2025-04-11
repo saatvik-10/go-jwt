593fbf72e0a9928840bd8729bad52250a6c273e5
saatvik-10 <saatvikmadan529@gmail.com>
2025-04-11 19:40:31 +0530

    added get user controller

--- controllers/authController.go
+++ controllers/authController.go
@@ -91,3 +91,25 @@ func Login(c *fiber.Ctx) error {
 		"message": "Logged in successfully",
 	})
 }
+
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	var user models.User
+	database.DB.Where("id = ?", claims.Issuer).First(&user)
+
+	return c.JSON(user)
+}
--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000",
 		AllowCredentials: true,
 	}))
 
